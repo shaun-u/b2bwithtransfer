b94ff1baf207c078ebbf1919d6b6b97c9cc52510
shaun-u <shaun.baker@utribo.com>
2014-05-27 12:23:06 +0200

    Basic DI and AmSession interface

---
+++ B2BTransFactory.h
@@ -0,0 +1,29 @@
+#ifndef _B2BTRANSFACTORY_H_
+#define _B2BTRANSFACTORY_H_
+
+#include "AmApi.h"
+
+class B2BTransFactory :
+  public AmSessionFactory, public AmDynInvokeFactory, public AmDynInvoke
+{
+  public:
+  CallTransFactory(const std::string& applicationName);
+  
+  int onLoad();
+
+  /**
+   * incoming call-leg entry point
+   * starts new b2b session
+   */
+  AmSession* onInvite(const AmSipRequest& req);
+  
+  /**
+   * Dynamic Invoke Methods
+   */
+  AmDynInvoke* getInstance();
+  void invoke(const string& method, const AmArg& args, AmArg& ret);
+
+  
+};
+
+#endif //_B2BTRANSFACTORY_H_
